a6f48ede650c423f3a93780627124e111397f010
Pedro Henrique <pedrohesm@gmail.com>
2025-10-03 21:50:42 -0300

    feat: add iac pulumi

---
+++ infra/index.ts
@@ -0,0 +1,45 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as awsx from '@pulumi/awsx';
+import * as aws from '@pulumi/aws';
+import * as docker from '@pulumi/docker-build';
+
+const serverECRRepository = new awsx.ecr.Repository('trakker-server-ecr', {
+	forceDelete: true,
+});
+
+const ecrTokenServer = aws.ecr.getAuthorizationTokenOutput({
+	registryId: serverECRRepository.repository.registryId,
+});
+
+export const serverDockerImage = new docker.Image('trakker-server-image', {
+	tags: [
+		pulumi.interpolate`${serverECRRepository.repository.repositoryUrl}:latest`,
+	],
+	context: {
+		location: '../',
+	},
+	push: true,
+	platforms: ['linux/amd64'],
+	registries: [
+		{
+			address: serverECRRepository.repository.repositoryUrl,
+			username: ecrTokenServer.userName,
+			password: ecrTokenServer.password,
+		},
+	],
+});
+
+const cluster = new awsx.classic.ecs.Cluster('trakker-cluster');
+
+const trakkerService = new awsx.classic.ecs.FargateService('trakker-service', {
+	cluster,
+	desiredCount: 1,
+	waitForSteadyState: false,
+	taskDefinitionArgs: {
+		container: {
+			image: serverDockerImage.ref,
+			cpu: 256,
+			memory: 512,
+		},
+	},
+});
